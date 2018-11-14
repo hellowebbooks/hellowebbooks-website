554b8c45dea5f8ceafe6d079a4d819beabc43b51
Tracy Osborn <tracy@weddinglovely.com>
2018-11-14 16:18:03 -0500

    Added fabric file

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+import os
+from fabric import task, Connection
+
+@task
+def deploy(c):
+    c = Connection(host='165.227.33.196', user=os.environ['POSTGRES_US'])
+    with c.cd('hellowebbooks/hellowebbooks'):
+        c.run('git pull origin master')
