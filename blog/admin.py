26c82733d1daba0f1764bff111751596c6e4a1cb
Tracy Osborn <tracy@weddinglovely.com>
2018-11-13 21:28:34 -0500

    Added blogpage to admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import BlogPage
+
+# Register your models here.
+class BlogPageAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(BlogPage, BlogPageAdmin)
