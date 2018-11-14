4494dadfe1b9a13636adaf8909d4e41649ed74e5
Tracy Osborn <tracy@weddinglovely.com>
2018-11-14 16:54:49 -0500

    Updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,18 @@
+import getpass
 import os
-from fabric import task, Connection
+from fabric import task, Connection, Config
+
 
 @task
 def deploy(c):
-    c = Connection(host='165.227.33.196', user=os.environ['POSTGRES_US'])
+    sudo_pass = os.environ.get('SUDO_PW')
+    if not sudo_pass:
+        sudo_pass = getpass.getpass("sudo password? (or set $SUDO_PW): ")
+
+    config = Config(overrides={'sudo': {'password': sudo_pass}})
+    c = Connection(host='165.227.33.196', user=os.environ['POSTGRES_US'], config=config)
     with c.cd('hellowebbooks/hellowebbooks'):
         c.run('git pull origin master')
-        c.sudo('systemctl restart gunicorn')
+
+    c.sudo('systemctl restart gunicorn')
+    c.sudo('journalctl -u gunicorn --since "1 min ago"')
