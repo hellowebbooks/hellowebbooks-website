1bad7a4b35cfb3d8561cf7f6da32c34e2a84d122
Tracy Osborn <tracy@weddinglovely.com>
2018-11-27 12:27:54 -0800

    Added models for Product, Customer, and Membership

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Customer, Membership, Product
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer
+
+class MembershipAdmin(admin.ModelAdmin):
+    model = Membership
+    list_display = ('customer', 'product', 'paperback', 'video',)
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Product, ProductAdmin)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Timestamp(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Customer(Timestamp):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+        related_name="customers")
+    stripe_id = models.CharField(max_length=255, blank=True)
+    last_4_digits = models.CharField(max_length=255, blank=True)
+    coupon = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.user.email
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    customers = models.ManyToManyField(Customer, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(Timestamp):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,
+        related_name="customers")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+        related_name="products")
+    paperback = models.BooleanField(default=False)
+    video = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.customer.user.username
