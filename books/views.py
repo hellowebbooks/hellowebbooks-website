96806c06b078efcc674fe1745addeec4a34c3b95
Tracy Osborn <tracy@weddinglovely.com>
2019-02-13 14:15:42 -0500

    Fixed the blog ordering on the homepage

--- books/views.py
+++ books/views.py
@@ -5,7 +5,10 @@ from blog.models import PostPage
 
 # Create your views here.
 def index(request):
-    posts = PostPage.objects.select_related().all()
+    posts = PostPage.objects.select_related().all().order_by('-date')
+
+    for post in posts:
+        print(post.date)
 
     return render(request, 'index.html', {
         'posts': posts,
