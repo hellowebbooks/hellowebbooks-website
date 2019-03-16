09cae043ab014655278d80a66eb751c5069f8c44
Tracy Osborn <tracy@weddinglovely.com>
2019-03-16 16:04:44 -0400

    Loading num customers into admin dashboard

--- books/views/admin_views.py
+++ books/views/admin_views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
+from books.models import Customer
+
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_dashboard(request):
+    customer_count = Customer.objects.count()
     return render(request, 'admin/dashboard.html', {
+        'customer_count': customer_count,
     })
