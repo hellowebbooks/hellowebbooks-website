a79f6a056634d80c00548204ffb0fae3be652220
Tracy Osborn <tracy@weddinglovely.com>
2019-03-26 10:13:18 -0400

    Added searching to admin

--- books/admin.py
+++ books/admin.py
@@ -6,10 +6,12 @@ from books.models import Customer, Membership, Product
 class CustomerAdmin(admin.ModelAdmin):
     model = Customer
     list_display = ('user_email', 'stripe_id', 'last_4_digits', 'coupon', 'gift',)
+    search_fields = ('user__email', 'stripe_id', 'coupon',)
 
 class MembershipAdmin(admin.ModelAdmin):
     model = Membership
     list_display = ('customer', 'product', 'paperback', 'video',)
+    search_fields = ('customer__user__email',)
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
