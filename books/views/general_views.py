8f6f8a90879253d37077da66bfea111b1d5c0300
Tracy Osborn <tracy@weddinglovely.com>
2019-04-11 15:04:34 -0400

    Forgot import

--- books/views/general_views.py
+++ books/views/general_views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
+from django.core.mail import mail_admins
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
